925916da6460e8c7e5ab42be0e29f7909936078e
Ingi Helgason <35065616+vecless@users.noreply.github.com>
2022-01-11 17:06:38 -0800

    Add ExampleTest

---
+++ src/test/java/frc/robot/ExampleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onePlusOneIsTwo() {
+        assertEquals(1+1, 2);
+    }
+}
