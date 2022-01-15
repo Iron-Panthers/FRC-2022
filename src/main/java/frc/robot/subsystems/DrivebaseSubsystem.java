a801887bb8ae6eb189db83ba4f431693be950526
Isaac <isaac@rubenfamily.com>
2022-01-15 10:33:49 -0800

    stop using m_, continue copying code

--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -7,13 +7,17 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.Drive.*;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.swervedrivespecialties.swervelib.SwerveModule;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DrivebaseSubsystem extends SubsystemBase {
-  private final SwerveDriveKinematics m_kinematics =
+  private final SwerveDriveKinematics kinematics =
       new SwerveDriveKinematics(
           // Front left
           new Translation2d(Dims.TRACKWIDTH_METERS / 2.0, Dims.WHEELBASE_METERS / 2.0),
@@ -26,8 +30,17 @@ public class DrivebaseSubsystem extends SubsystemBase {
 
   private final AHRS m_navx = new AHRS(SPI.Port.kMXP, (byte) 200); // NavX connected over MXP
 
+  private final SwerveModule frontLeftModule;
+  private final SwerveModule frontRightModule;
+  private final SwerveModule backLeftModule;
+  private final SwerveModule backRightModule;
+
+  private ChassisSpeeds chassisSpeeds = new ChassisSpeeds(); // defaults to zeros
+
   /** Creates a new DrivebaseSubsystem. */
-  public DrivebaseSubsystem() {}
+  public DrivebaseSubsystem() {
+    ShuffleboardTab tab = Shuffleboard.getTab("Drivebase");
+  }
 
   @Override
   public void periodic() {
