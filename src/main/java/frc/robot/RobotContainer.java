b7de9acab7ded903fe0eee9c600faa190ff81534
Ingi Helgason <35065616+vecless@users.noreply.github.com>
2022-01-27 15:04:56 -0800

    Add example use of FollowTrajectoryCommand into RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,12 @@ package frc.robot;
 import static frc.robot.Constants.Drive;
 
 import com.ironpanthers.lib.Util;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -14,7 +20,9 @@ import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.commands.DefenseModeCommand;
+import frc.robot.commands.FollowTrajectoryCommand;
 import frc.robot.subsystems.DrivebaseSubsystem;
+import java.util.List;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -67,8 +75,25 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return null;
+    TrajectoryConfig config =
+        new TrajectoryConfig(
+                Constants.Drive.MAX_VELOCITY_METERS_PER_SECOND,
+                0.5 * Constants.Drive.MAX_VELOCITY_METERS_PER_SECOND)
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(drivebaseSubsystem.getKinematics());
+
+    // An example trajectory to follow.  All units in meters.
+    Trajectory exampleTrajectory =
+        TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            // Pass through these two interior waypoints, making an 's' curve path
+            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+            // End 3 meters straight ahead of where we started, facing forward
+            new Pose2d(3, 0, new Rotation2d(0)),
+            config);
+
+    return new FollowTrajectoryCommand(exampleTrajectory, drivebaseSubsystem);
   }
 
   /**
