38012d117d02ec7d339e4313e1637c83afb1a8fc
Isaac <isaac@rubenfamily.com>
2022-02-10 16:16:53 -0800

    prep to unit test motors

--- src/test/java/frc/robot/IntakeSubsystemTest.java
+++ src/test/java/frc/robot/IntakeSubsystemTest.java
@@ -3,17 +3,31 @@ package frc.robot;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.hal.HAL;
 import frc.RobotTest;
 import frc.robot.subsystems.IntakeSubsystem;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class IntakeSubsystemTest {
 
   private IntakeSubsystem intakeSubsystem;
+  private TalonFX lowerMotor;
+  private TalonFX upperMotor;
+  private TalonFX idlerMotor;
 
   @BeforeEach
-  public void reset() {
+  public void setup() {
+    // timeout, mode
+    // timeout in ms
+    assert HAL.initialize(500, 0);
     intakeSubsystem = new IntakeSubsystem();
+  }
+
+  @AfterEach
+  public void shutdown() {
+    intakeSubsystem.close();
   }
 
   @RobotTest
