b87b68617dd136d1aca7dceeb82483b2cc2767f5
I H <35065616+vecless@users.noreply.github.com>
2022-02-11 10:31:24 -0800

    Restore comment in robotInit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,6 +25,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
   }
 
