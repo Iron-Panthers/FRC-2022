cd9fb31b550302e3b48c75d9ec8b743abb24ea60
Isaac <isaac@rubenfamily.com>
2022-02-12 19:06:08 -0800

    rotate with velocity command

---
+++ src/main/java/frc/robot/commands/RotateVelocityDriveCommand.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivebaseSubsystem;
+import java.util.function.DoubleSupplier;
+
+public class RotateVelocityDriveCommand extends CommandBase {
+  private final DrivebaseSubsystem drivebaseSubsystem;
+
+  private final DoubleSupplier translationXSupplier;
+  private final DoubleSupplier translationYSupplier;
+  private final DoubleSupplier rotationSupplier;
+
+  /** Creates a new RotateVelocityDriveCommand. */
+  public RotateVelocityDriveCommand(
+      DrivebaseSubsystem drivebaseSubsystem,
+      DoubleSupplier translationXSupplier,
+      DoubleSupplier translationYSupplier,
+      DoubleSupplier rotationSupplier) {
+
+    this.drivebaseSubsystem = drivebaseSubsystem;
+    this.translationXSupplier = translationXSupplier;
+    this.translationYSupplier = translationYSupplier;
+    this.rotationSupplier = rotationSupplier;
+
+    addRequirements(drivebaseSubsystem);
+  }
+
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double x = translationXSupplier.getAsDouble();
+    double y = translationYSupplier.getAsDouble();
+    double rot = rotationSupplier.getAsDouble();
+
+    // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented
+    // movement
+    drivebaseSubsystem.drive(
+        ChassisSpeeds.fromFieldRelativeSpeeds(
+            x, y, rot, drivebaseSubsystem.getGyroscopeRotation()));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivebaseSubsystem.drive(new ChassisSpeeds());
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
