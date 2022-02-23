54abdcd4d1ab67a9e332b0bc7569ee0cbeab21b5
Jayden <jdenwan@gmail.com>
2022-02-22 18:03:47 -0800

    Added PIDController
    This is still something that needs to get worked on further, just using the opportunity to define a PIDController

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
+
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,6 +19,7 @@ import frc.robot.Constants;
     Todo:
         1. Make and initialize two motors *DONE*
         2. Define functions in this subsystem to move the arm a direction
+          2a. Figure out how to do some cool PID stuff
         3. Make a command ArmCommand.java that has handles moving the arm
         4. Define certain buttons to call command functions that move our arm some amount of degrees
             4a. We should have encoders to be able to move by degrees somehow
@@ -24,13 +27,20 @@ import frc.robot.Constants;
  */
 
 public class ArmSubsystem extends SubsystemBase {
+<<<<<<< Updated upstream
   private TalonFX armMotorOne;
   private TalonFX armMotorTwo;
+=======
+  private static TalonFX armMotorOne;
+  private static TalonFX armMotorTwo;
+  private static PIDController pidController;
+>>>>>>> Stashed changes
 
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
     armMotorOne = new TalonFX(Constants.Arm.MOTOR_1_PORT);//FIX LATER
     armMotorTwo = new TalonFX(Constants.Arm.MOTOR_2_PORT);//FIX LATER
+    pidController = new PIDController(1.0, 1.0, 1.0);               // TODO: Look into having to edit the PID Values to be propert
   }
 
   public void setPower(double Power) {
