c041461a8144a0a812edd69dca4cdafe2c1f2123
Ariel Pawlik-Zwiebel <arielpz123@yahoo.com>
2022-02-22 19:10:59 -0800

    Added various arm command files

---
+++ src/main/java/frc/robot/commands/Arm/ArmMoveUp.java
@@ -0,0 +1,37 @@
+package frc.robot.commands.Arm;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class ArmMoveUp extends CommandBase {
+    ArmSubsystem arm;
+    
+    /**
+     * Creates a new ArmCommand
+     */
+    public ArmMoveUp(ArmSubsystem arm) {
+        this.arm = arm;
+    }
+
+    // Called when command is initially scheduled
+    @Override
+    public void initialize() {
+    }
+
+    // Called when scheduler runs when command is initially scheduled
+    @Override
+    public void execute() {
+    }
+
+    // Called once the command ends or is interrupted
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    // 
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
