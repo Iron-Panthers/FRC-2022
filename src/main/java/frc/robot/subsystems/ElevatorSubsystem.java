1db03078d8d19bf2334f51fc28e0bf78090d00bf
Brandon <25nguyenb26@stu.smuhsd.org>
2022-02-22 16:57:49 -0800

    Made elevator subsystem
    Also added elevator class to constants

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ElevatorSubsystem extends SubsystemBase {
+  private final TalonFX left = new TalonFX(Constants.Elevator.ELEVATOR_MOTOR);
+  private final TalonFX right = new TalonFX(Constants.Elevator.ELEVATOR_MOTOR_2);
+
+  // clockwise moves up
+  // counterclockwise moves down
+
+  /** Creates a new ElevatorSubsystem. */
+  public ElevatorSubsystem() {
+
+    // Configure the right motor
+    right.setInverted(true);
+
+    // Follow with the left motor and copy any important things
+    left.follow(right);
+    left.setInverted(TalonFXInvertType.FollowMaster);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
