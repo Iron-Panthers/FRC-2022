8a7229f638f0583193aeb23d6f3eb012800e5bb6
Ariel Pawlik-Zwiebel <arielpz123@yahoo.com>
2022-02-22 18:51:13 -0800

    Fixed the class definition
    Added CommandBase

--- src/main/java/frc/robot/commands/ArmCommand.java
+++ src/main/java/frc/robot/commands/ArmCommand.java
@@ -1,8 +1,9 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ArmSubsystem;
 
-public class ArmCommand {
+public class ArmCommand extends CommandBase {
     ArmSubsystem arm;
     
     /**
