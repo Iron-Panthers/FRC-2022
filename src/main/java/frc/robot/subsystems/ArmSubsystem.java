5cb4b58156e83deb14891e4efad920e6f224af82
Landon S <landon.stobaugh@gmail.com>
2022-02-22 16:34:42 -0800

    Re:Init
    Helped setup a subsystem a bit more efficiently

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,3 +1,7 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,10 +17,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
  */
 
-public class ArmSubsystem extends SubsystemBase 
-{
- public ArmSubsystem()
- {
+public class ArmSubsystem extends SubsystemBase {
+  /** Creates a new ArmSubsystem. */
+  public ArmSubsystem() {}
 
- }   
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
