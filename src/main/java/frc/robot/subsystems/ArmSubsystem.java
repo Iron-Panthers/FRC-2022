485c3f0038efec2b4db18a004d12c70ca5406a94
Landon S <landon.stobaugh@gmail.com>
2022-02-22 16:32:29 -0800

    Init of ArmSubsystem
    Just getting us started with a Subsystem file and a todo list of what we need to get done

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/*
+
+    Todo:
+        1. Make and initialize two motors
+        2. Define functions in this subsystem to move the arm a direction
+        3. Make a command ArmCommand.java that has handles moving the arm
+        4. Define certain buttons to call command functions that move our arm some amount of degrees
+            4a. We should have encoders to be able to move by degrees somehow
+
+ */
+
+public class ArmSubsystem extends SubsystemBase 
+{
+ public ArmSubsystem()
+ {
+
+ }   
+}
