f69dcbbe439819083a632042d1be06824b4c00c4
Jayden <jdenwan@gmail.com>
2022-02-24 17:22:24 -0800

    Ingi fixed stuff

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -6,21 +6,16 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-<<<<<<< HEAD
-=======
 
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-
-
-
->>>>>>> Arm
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 /*
-
    Todo:
        1. Make and initialize two motors *DONE*
        2. Define functions in this subsystem to move the arm a direction
@@ -30,12 +25,14 @@ import frc.robot.Constants;
            4a. We should have encoders to be able to move by degrees somehow
 
 */
-
 public class ArmSubsystem extends SubsystemBase {
-  private TalonFX armMotorOne;
-  private TalonFX armMotorTwo;
-  private PIDController pidController;
-  private double DesiredAngle = Constants.Arm.PID.ANGULAR_OFFSET;
+  private final TalonFX armMotorOne;
+  private final TalonFX armMotorTwo;
+  private final PIDController pidController;
+
+  private final CANCoder armEncoder;
+
+  private double desiredAngle = Constants.Arm.PID.ANGULAR_OFFSET;
 
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
@@ -43,43 +40,40 @@ public class ArmSubsystem extends SubsystemBase {
     armMotorTwo = new TalonFX(Constants.Arm.Ports.MOTOR_2_PORT); // FIX LATER
 
     pidController = new PIDController(0.01, 0, 0.01);
-    pidController.setSetpoint(0.0);
     pidController.setTolerance(Constants.Arm.PID.ANGULAR_TOLERANCE);
+
+    armEncoder = new CANCoder(200); // FIXME: we will need to figure out the real value
+    armEncoder.configFactoryDefault();
+    armEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
   }
 
-  public void setPower(
-      double Power) { // We probably will not be using this too much, more focused into using
-    // positional values
-    armMotorOne.set(TalonFXControlMode.PercentOutput, Power);
-    armMotorTwo.set(TalonFXControlMode.PercentOutput, -Power);
+  public void setPower(double power) {
+    // We probably will not be using this too much, more focused into using positional values
+    armMotorOne.set(TalonFXControlMode.PercentOutput, power);
+    armMotorTwo.set(TalonFXControlMode.PercentOutput, -power);
   }
 
-<<<<<<< HEAD
+  // Sets the goal of the pid controller
   public void setAngle(double dAngle) {
-    DesiredAngle = dAngle; // Set the setpoint of the PIDController
-    pidController.setSetpoint(DesiredAngle);
-=======
-  public void setAngle(double Angle) {
-    
->>>>>>> Arm
+    desiredAngle = dAngle; // Set the setpoint of the PIDController
   }
 
   public void stopMotor(){
     setPower(0);
   }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    double currentAngle = armEncoder.getAbsolutePosition();
 
-    // If we're not moving towards a point, we should probably reset the PIDController
-    double currentAngle =
-        armMotorOne.getSelectedSensorPosition() * Constants.Arm.PID.TICKS_TO_DEGREES;
+    // double output = controller.calculate(measurement (what is actually there), desired value (where we want it to be))
+    // -> PID math gibberish -> the output we want to write to our motor(s)
 
-    double rotationValue = pidController.calculate(currentAngle - DesiredAngle);
+    final double output = pidController.calculate(currentAngle, desiredAngle);
+    final double clampedOutput = MathUtil.clamp(output, -1, 1);
 
-    rotationValue = MathUtil.clamp(rotationValue, -1, 1);
-
-    setPower(rotationValue);
+    setPower(clampedOutput);
     // Util.relativeAngularDifference();
   }
 }
