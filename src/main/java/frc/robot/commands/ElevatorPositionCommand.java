944c2e5d8f086b082e0f7c2511621f5343f196f5
Brandon <25nguyenb26@stu.smuhsd.org>
2022-02-26 15:01:32 -0800

    Changed Button Bindings
    now more intuitive
    Y - manually makes elevator rise
    A - manually makes elevator go down
    B - Elevator automatically extends to maximum height
    X - Automatically goes to minimum height

--- src/main/java/frc/robot/commands/ElevatorPositionCommand.java
+++ src/main/java/frc/robot/commands/ElevatorPositionCommand.java
@@ -1,12 +1,12 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
- 
+
 package frc.robot.commands;
- 
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ElevatorSubsystem;
- 
+
 public class ElevatorPositionCommand extends CommandBase {
   private final ElevatorSubsystem elevatorSubsystem;
   private Double targetHeight;
@@ -17,35 +17,33 @@ public class ElevatorPositionCommand extends CommandBase {
     this.targetHeight = targetHeight;
     // Use addRequirements() here to declare subsystem dependencies.
   }
- 
+
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
- 
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     elevatorSubsystem.setTargetHeight(targetHeight);
     elevatorSubsystem.periodic();
- 
+
     // get current height
   }
- 
+
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-   
-  }
- 
+  public void end(boolean interrupted) {}
+
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
   }
 }
- 
+
 // 12.75 full motor rotations = 1.5pi inches of height
- 
+
 // 12.75:1 gear ratio
 // talonfx to the entire thing
 // Big gear is 2.6 inches
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,9 +1,9 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
- 
+
 package frc.robot.subsystems;
- 
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.Elevator;
- 
+
 public class ElevatorSubsystem extends SubsystemBase {
   private final TalonFX left = new TalonFX(Constants.Elevator.Ports.LEFT_MOTOR);
   private final TalonFX right = new TalonFX(Constants.Elevator.Ports.RIGHT_MOTOR);
@@ -21,34 +21,36 @@ public class ElevatorSubsystem extends SubsystemBase {
       new DigitalInput(Elevator.Ports.BOTTOM_SWITCH); // do we need to put constants?
   private final DigitalInput topLimitSwitch =
       new DigitalInput(Elevator.Ports.TOP_SWITCH); // do we need to put constants?
- 
+
   private double totalMotorRotationTicks;
   private double motorRotations;
-  private double absoluteHeight;
+  /** Elevator's current height in inches */
+  private double currentHeight;
+
   private double targetHeight;
   private double motorPower;
   private final PIDController heightController = new PIDController(0.04, 0, 0);
- 
+
   // clockwise moves up
   // counterclockwise moves down
- 
+
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem() {
- 
+
     this.totalMotorRotationTicks = 0.0;
-    this.absoluteHeight = 0.0;
+    this.currentHeight = 0.0;
     // Configure the right motor
     right.setInverted(true);
- 
+
     // Follow with the left motor and copy any important things
     left.follow(right);
     left.setInverted(TalonFXInvertType.FollowMaster);
   }
- 
+
   public void setMotorPower(Double power) {
     right.set(TalonFXControlMode.PercentOutput, power);
   }
- 
+
   /**
    * Stores the target height for the elevator, to be reached with motor adjustment
    *
@@ -57,14 +59,14 @@ public class ElevatorSubsystem extends SubsystemBase {
   public void setTargetHeight(Double targetHeight) {
     this.targetHeight = targetHeight;
   }
- 
+
   /**
-   * Calculate absolute height of elevator
+   * Calculate current height of elevator
    *
-   * @return Absolute Height in inches
+   * @return current Height in inches
    */
   public double getHeight() {
- 
+
     /* Lexie's code
         if (motorPower >= 0) {
           if (currentMotorRotation < pastMotorRotation) {
@@ -78,47 +80,47 @@ public class ElevatorSubsystem extends SubsystemBase {
           this.totalMotorRotation += (currentMotorRotation - pastMotorRotation);
         }
     */
- 
+
     if (bottomLimitSwitchPressed()) {
       this.totalMotorRotationTicks = 0;
     } else {
       this.totalMotorRotationTicks = right.getSelectedSensorPosition();
     }
- 
+
     this.motorRotations = this.totalMotorRotationTicks / 4096; // There are 4096 units per rotation
- 
-    this.absoluteHeight =
+
+    this.currentHeight =
         ((motorRotations / 12.75) // 12.75:1 gear ratio
             * (1.5 * Math.PI)); // Circumference of gear multiplied by rotations to get height
- 
-    return absoluteHeight;
+
+    return currentHeight;
   }
- 
+
   public static boolean epsilonEquals(double a, double b, double epsilon) {
     return (a - epsilon <= b) && (a + epsilon >= b);
   }
- 
+
   public void lock() {
- 
+
     right.setNeutralMode(NeutralMode.Brake);
   }
- 
+
   public boolean topLimitSwitchPressed() {
- 
+
     return topLimitSwitch.get();
   }
- 
+
   public boolean bottomLimitSwitchPressed() {
- 
+
     return bottomLimitSwitch.get();
   }
- 
+
   @Override
   public void periodic() {
-    double currentHeight = getHeight();
+    currentHeight = getHeight();
     this.motorPower = heightController.calculate(getHeight(), targetHeight);
     right.set(TalonFXControlMode.PercentOutput, motorPower);
-    if (epsilonEquals(currentHeight, targetHeight, .1)){
+    if (epsilonEquals(currentHeight, targetHeight, .1)) {
       lock();
     }
     if (topLimitSwitchPressed() || bottomLimitSwitchPressed()) {
@@ -127,7 +129,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 }
- 
+
 // 12.75 full motor rotations = 1.5pi inches of height
 // 12.75:1 gear ratio
 // talonfx to the entire thing
