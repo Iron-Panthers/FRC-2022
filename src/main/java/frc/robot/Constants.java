c1e4a1b67eb07bb82945560a123a962f0e705f45
driverstation <5026craptop@gmail.com>
2022-02-28 16:34:31 -0800

    give a little more gravity

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -105,7 +105,7 @@ public final class Constants {
     }
 
     public static final double ANGULAR_OFFSET = 71;
-    public static final double GRAVITY_CONTROL_PERCENT = .13;
+    public static final double GRAVITY_CONTROL_PERCENT = .145;
 
     public static final class PID {
       public static final double ANGULAR_TOLERANCE = 1.0;
