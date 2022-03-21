fb68448f7c4f10ba44cdf0959dcf5fbc863a251c
I H <35065616+vecless@users.noreply.github.com>
2022-03-21 15:34:49 -0700

    Remove unused blocks from new intake commands

--- src/main/java/frc/robot/commands/ForceIntakeModeCommand.java
+++ src/main/java/frc/robot/commands/ForceIntakeModeCommand.java
@@ -25,10 +25,6 @@ public class ForceIntakeModeCommand extends CommandBase {
     intakeSubsystem.setLockMode(mode);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
