47a7a934c86ea2ec64f7aea25b1304adbd3f5e71
I H <35065616+vecless@users.noreply.github.com>
2022-03-25 12:51:39 -0700

    Make DoNotUse autonomous unusable

--- src/main/java/frc/robot/autonomous/commands/GreedyOnsideAutoSequence.java
+++ src/main/java/frc/robot/autonomous/commands/GreedyOnsideAutoSequence.java
@@ -8,16 +8,10 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants.Arm;
 import frc.robot.autonomous.Waypoints.OnsideStartToInnerCargo;
 import frc.robot.autonomous.Waypoints.OnsideStartToOuterCargo;
 import frc.robot.autonomous.Waypoints.OnsideStartToTerminalTwoCargo;
-import frc.robot.commands.FollowTrajectoryCommand;
-import frc.robot.commands.ForceIntakeModeCommand;
-import frc.robot.commands.SetIntakeModeCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DrivebaseSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -87,59 +81,60 @@ public class GreedyOnsideAutoSequence extends SequentialCommandGroup {
 
     // Sequence is explained by comments
     addCommands(
-        new InstantCommand(() -> drivebaseSubsystem.zeroGyroscope(), drivebaseSubsystem),
-        // Follow trajectory and intake after a delay
-        // Lower arm once so as to not induce too much dive at the beginning
-        parallel(
-            sequence(
-                new WaitCommand(0.8), // TODO: tune
-                new InstantCommand(
-                    () -> armSubsystem.setAngle(Arm.Setpoints.INTAKE_POSITION), armSubsystem)),
-            deadline(
-                new FollowTrajectoryCommand(onsideToOuterCargo, true, drivebaseSubsystem),
-                sequence(
-                    new WaitCommand(0.25),
-                    new ForceIntakeModeCommand(intakeSubsystem, IntakeSubsystem.Modes.INTAKE)))),
-        new FollowTrajectoryCommand(outerCargoToOnside, drivebaseSubsystem),
-        // Once we're back at the start pose, raise the arm to the scoring position
-        deadline(
-            new WaitCommand(0.75),
-            new InstantCommand(
-                () -> armSubsystem.setAngle(Arm.Setpoints.OUTTAKE_HIGH_POSITION - 10),
-                armSubsystem)),
-        // Score the 2 cargo
-        new SetIntakeModeCommand(
-            intakeSubsystem, IntakeSubsystem.Modes.ALIGN_HIGH, IntakeSubsystem.Modes.OFF),
-        // GOTO INNER CARGO
-        parallel(
-            sequence(
-                new WaitCommand(0.8), // TODO: tune
-                new InstantCommand(
-                    () -> armSubsystem.setAngle(Arm.Setpoints.INTAKE_POSITION), armSubsystem)),
-            deadline(
-                new FollowTrajectoryCommand(onsideToInnerCargo, true, drivebaseSubsystem),
-                sequence(
-                    new WaitCommand(0.25),
-                    new ForceIntakeModeCommand(intakeSubsystem, IntakeSubsystem.Modes.INTAKE)))),
-        new FollowTrajectoryCommand(innerCargoToOnside, drivebaseSubsystem),
-        // SCORE INNER CARGO
-        new SetIntakeModeCommand(
-            intakeSubsystem, IntakeSubsystem.Modes.ALIGN_HIGH, IntakeSubsystem.Modes.OFF),
-        // GOTO TERMINAL
-        parallel(
-            sequence(
-                new WaitCommand(1), // TODO: tune
-                new InstantCommand(
-                    () -> armSubsystem.setAngle(Arm.Setpoints.INTAKE_POSITION), armSubsystem)),
-            deadline(
-                new FollowTrajectoryCommand(onsideToTerminalTwoCargo, true, drivebaseSubsystem),
-                sequence(
-                    new WaitCommand(0.25),
-                    new ForceIntakeModeCommand(intakeSubsystem, IntakeSubsystem.Modes.INTAKE)))),
-        new FollowTrajectoryCommand(terminalTwoCargoToOnside, drivebaseSubsystem),
-        // no way this happens in time with this revision, but...
-        // SCORE TERMINAL CARGO
-        new SetIntakeModeCommand(
-            intakeSubsystem, IntakeSubsystem.Modes.ALIGN_HIGH, IntakeSubsystem.Modes.OFF));
+        // new InstantCommand(() -> drivebaseSubsystem.zeroGyroscope(), drivebaseSubsystem),
+        // // Follow trajectory and intake after a delay
+        // // Lower arm once so as to not induce too much dive at the beginning
+        // parallel(
+        //     sequence(
+        //         new WaitCommand(0.8), // TODO: tune
+        //         new InstantCommand(
+        //             () -> armSubsystem.setAngle(Arm.Setpoints.INTAKE_POSITION), armSubsystem)),
+        //     deadline(
+        //         new FollowTrajectoryCommand(onsideToOuterCargo, true, drivebaseSubsystem),
+        //         sequence(
+        //             new WaitCommand(0.25),
+        //             new ForceIntakeModeCommand(intakeSubsystem, IntakeSubsystem.Modes.INTAKE)))),
+        // new FollowTrajectoryCommand(outerCargoToOnside, drivebaseSubsystem),
+        // // Once we're back at the start pose, raise the arm to the scoring position
+        // deadline(
+        //     new WaitCommand(0.75),
+        //     new InstantCommand(
+        //         () -> armSubsystem.setAngle(Arm.Setpoints.OUTTAKE_HIGH_POSITION - 10),
+        //         armSubsystem)),
+        // // Score the 2 cargo
+        // new SetIntakeModeCommand(
+        //     intakeSubsystem, IntakeSubsystem.Modes.ALIGN_HIGH, IntakeSubsystem.Modes.OFF),
+        // // GOTO INNER CARGO
+        // parallel(
+        //     sequence(
+        //         new WaitCommand(0.8), // TODO: tune
+        //         new InstantCommand(
+        //             () -> armSubsystem.setAngle(Arm.Setpoints.INTAKE_POSITION), armSubsystem)),
+        //     deadline(
+        //         new FollowTrajectoryCommand(onsideToInnerCargo, true, drivebaseSubsystem),
+        //         sequence(
+        //             new WaitCommand(0.25),
+        //             new ForceIntakeModeCommand(intakeSubsystem, IntakeSubsystem.Modes.INTAKE)))),
+        // new FollowTrajectoryCommand(innerCargoToOnside, drivebaseSubsystem),
+        // // SCORE INNER CARGO
+        // new SetIntakeModeCommand(
+        //     intakeSubsystem, IntakeSubsystem.Modes.ALIGN_HIGH, IntakeSubsystem.Modes.OFF),
+        // // GOTO TERMINAL
+        // parallel(
+        //     sequence(
+        //         new WaitCommand(1), // TODO: tune
+        //         new InstantCommand(
+        //             () -> armSubsystem.setAngle(Arm.Setpoints.INTAKE_POSITION), armSubsystem)),
+        //     deadline(
+        //         new FollowTrajectoryCommand(onsideToTerminalTwoCargo, true, drivebaseSubsystem),
+        //         sequence(
+        //             new WaitCommand(0.25),
+        //             new ForceIntakeModeCommand(intakeSubsystem, IntakeSubsystem.Modes.INTAKE)))),
+        // new FollowTrajectoryCommand(terminalTwoCargoToOnside, drivebaseSubsystem),
+        // // no way this happens in time with this revision, but...
+        // // SCORE TERMINAL CARGO
+        // new SetIntakeModeCommand(
+        //     intakeSubsystem, IntakeSubsystem.Modes.ALIGN_HIGH, IntakeSubsystem.Modes.OFF)
+        );
   }
 }
