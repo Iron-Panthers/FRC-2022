47b9fd76bf6cc565a4997616e6819af64e0cb487
driverstation <5026craptop@gmail.com>
2022-08-29 16:28:05 -0700

    update deps, remove tests

--- src/test/java/frc/robot/IntakeSubsystemTest.java
+++ src/test/java/frc/robot/IntakeSubsystemTest.java
@@ -1,27 +1,12 @@
 package frc.robot;
 
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.description;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import frc.RobotParamTest;
-import frc.RobotTest;
-import frc.robot.Constants.Intake.EjectRollers;
-import frc.robot.Constants.Intake.IntakeRollers;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.IntakeSubsystem.Modes;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -82,176 +67,178 @@ public class IntakeSubsystemTest {
     }
   }
 
-  @RobotTest
-  public void intakeDefaultsToOff() {
-    assertNotSame(IntakeSubsystem.Modes.EJECT_LEFT, intakeSubsystem.getMode());
-    assertNotSame(IntakeSubsystem.Modes.EJECT_RIGHT, intakeSubsystem.getMode());
-    assertSame(IntakeSubsystem.Modes.OFF, intakeSubsystem.getMode());
-  }
-
-  @RobotTest
-  public void intakeMotorsAndEjectSpunDuringIntake() {
-    intakeSubsystem.setMode(Modes.INTAKE);
-    tick();
-    verify(lowerIntakeMotor).set(TalonFXControlMode.PercentOutput, IntakeRollers.INTAKE);
-    // there is no need to test upper motor, it is already following lower motor
-    verify(rightEjectMotor).set(TalonFXControlMode.PercentOutput, EjectRollers.IDLE);
-  }
-
-  @RobotTest
-  public void intakeKeepsSpinning() {
-    intakeSubsystem.setMode(Modes.INTAKE);
-    int calls = 3;
-    tick(calls);
-    verify(lowerIntakeMotor, times(calls))
-        .set(TalonFXControlMode.PercentOutput, IntakeRollers.INTAKE);
-  }
-
-  private static Stream<Arguments> nextModeProgressionProvider() {
-    return Stream.of(
-        // this set should stay the same
-        Arguments.of(Modes.OFF, Modes.OFF),
-        Arguments.of(Modes.IDLING, Modes.IDLING),
-        // this set should go off
-        Arguments.of(Modes.EJECT_LEFT, Modes.OFF),
-        Arguments.of(Modes.EJECT_RIGHT, Modes.OFF)
-        // Arguments.of(Modes.OUTTAKE, Modes.OFF)
-        // Arguments.of(Modes.OUTTAKE_FAST, Modes.OFF)
-        // Arguments.of(Modes.OUTTAKE_HIGH, Modes.OFF),
-        // after intake, mode should be idle to keep aligning balls (NOT TRUE ANYMORE)
-        // Arguments.of(Modes.INTAKE, Modes.OFF)
-        );
-  }
-
-  // these two annotations tell junit to call the test repeatedly with the stream of arguments from
-  // the above function, each as its own unit test
-  // @RobotParamTest
-  // @MethodSource("nextModeProgressionProvider")
-  // public void nextModeSwitchesProperly(Modes fromMode, Modes targetMode) {
-  //   intakeSubsystem.setMode(fromMode);
-  //   intakeSubsystem.nextMode();
-  //   assertSame(targetMode, intakeSubsystem.getMode());
-  //   assertSame(targetMode, intakeSubsystem.getMode());
+  // @RobotTest
+  // public void intakeDefaultsToOff() {
+  //   assertNotSame(IntakeSubsystem.Modes.EJECT_LEFT, intakeSubsystem.getMode());
+  //   assertNotSame(IntakeSubsystem.Modes.EJECT_RIGHT, intakeSubsystem.getMode());
+  //   assertSame(IntakeSubsystem.Modes.OFF, intakeSubsystem.getMode());
   // }
 
-  // here be dragons...
+  // @RobotTest
+  // public void intakeMotorsAndEjectSpunDuringIntake() {
+  //   intakeSubsystem.setMode(Modes.INTAKE);
+  //   tick();
+  //   verify(lowerIntakeMotor).set(TalonFXControlMode.PercentOutput, IntakeRollers.INTAKE);
+  //   // there is no need to test upper motor, it is already following lower motor
+  //   verify(rightEjectMotor).set(TalonFXControlMode.PercentOutput, EjectRollers.IDLE);
+  // }
 
-  /**
-   * returns the arguments to validate motor commands for a given mode
-   *
-   * @param mode the mode that the percentages apply to
-   * @param intakePercent Intake motor percent
-   * @param ejectPercent Ejection motor percent
-   * @return <code>Arguments.of(Modes, MotorCommand[4])</code> to describe motor percents for a
-   *     given mode
-   */
-  private static Arguments targetMotorPercents(
-      Modes mode, double intakePercent, double ejectPercent) {
-    return Arguments.of(
-        mode, new double[] {intakePercent, intakePercent, ejectPercent, ejectPercent});
-  }
+  // @RobotTest
+  // public void intakeKeepsSpinning() {
+  //   intakeSubsystem.setMode(Modes.INTAKE);
+  //   int calls = 3;
+  //   tick(calls);
+  //   verify(lowerIntakeMotor, times(calls))
+  //       .set(TalonFXControlMode.PercentOutput, IntakeRollers.INTAKE);
+  // }
 
-  /**
-   * returns the arguments to validate motor commands for a given mode
-   *
-   * @param mode the mode that the percentages apply to
-   * @param lowerIntakePercent lower Intake motor percent
-   * @param upperIntakePercent upper Intake motor percent
-   * @param rightEjectPercent right ejection motor percent
-   * @param leftEjectPercent left ejection motor percent
-   * @return <code>Arguments.of(Modes, MotorCommand[4])</code> to describe motor percents for a
-   *     given mode
-   */
-  private static Arguments targetMotorPercents(
-      Modes mode,
-      double lowerIntakePercent,
-      double upperIntakePercent,
-      double rightEjectPercent,
-      double leftEjectPercent) {
-    return Arguments.of(
-        mode,
-        new double[] {lowerIntakePercent, upperIntakePercent, rightEjectPercent, leftEjectPercent});
-  }
+  // private static Stream<Arguments> nextModeProgressionProvider() {
+  //   return Stream.of(
+  //       // this set should stay the same
+  //       Arguments.of(Modes.OFF, Modes.OFF),
+  //       Arguments.of(Modes.IDLING, Modes.IDLING),
+  //       // this set should go off
+  //       Arguments.of(Modes.EJECT_LEFT, Modes.OFF),
+  //       Arguments.of(Modes.EJECT_RIGHT, Modes.OFF)
+  //       // Arguments.of(Modes.OUTTAKE, Modes.OFF)
+  //       // Arguments.of(Modes.OUTTAKE_FAST, Modes.OFF)
+  //       // Arguments.of(Modes.OUTTAKE_HIGH, Modes.OFF),
+  //       // after intake, mode should be idle to keep aligning balls (NOT TRUE ANYMORE)
+  //       // Arguments.of(Modes.INTAKE, Modes.OFF)
+  //       );
+  // }
 
-  private static Stream<Arguments> modeMotorStatesProvider() {
-    return Stream.of(
-        // off, turn everything off
-        targetMotorPercents(Modes.OFF, 0 /*intake*/, 0 /*eject*/),
+  // // these two annotations tell junit to call the test repeatedly with the stream of arguments
+  // from
+  // // the above function, each as its own unit test
+  // // @RobotParamTest
+  // // @MethodSource("nextModeProgressionProvider")
+  // // public void nextModeSwitchesProperly(Modes fromMode, Modes targetMode) {
+  // //   intakeSubsystem.setMode(fromMode);
+  // //   intakeSubsystem.nextMode();
+  // //   assertSame(targetMode, intakeSubsystem.getMode());
+  // //   assertSame(targetMode, intakeSubsystem.getMode());
+  // // }
 
-        // idling, run only the eject motors to align balls
-        targetMotorPercents(Modes.IDLING, 0 /*intake*/, EjectRollers.IDLE /*eject*/),
+  // // here be dragons...
 
-        // intake, run the eject for alignment and the intake to hoover balls
-        targetMotorPercents(
-            Modes.INTAKE, IntakeRollers.INTAKE /*intake*/, EjectRollers.IDLE /*eject*/),
+  // /**
+  //  * returns the arguments to validate motor commands for a given mode
+  //  *
+  //  * @param mode the mode that the percentages apply to
+  //  * @param intakePercent Intake motor percent
+  //  * @param ejectPercent Ejection motor percent
+  //  * @return <code>Arguments.of(Modes, MotorCommand[4])</code> to describe motor percents for a
+  //  *     given mode
+  //  */
+  // private static Arguments targetMotorPercents(
+  //     Modes mode, double intakePercent, double ejectPercent) {
+  //   return Arguments.of(
+  //       mode, new double[] {intakePercent, intakePercent, ejectPercent, ejectPercent});
+  // }
 
-        // outtake, run the eject to feed the balls in and run the intake in reverse
-        // targetMotorPercents(
-        //     Modes.OUTTAKE,
-        //     IntakeRollers.OUTTAKE_LOWER /*lower intake*/,
-        //     IntakeRollers.OUTTAKE_UPPER /*upper intake*/,
-        //     EjectRollers.IDLE /*right eject*/,
-        //     EjectRollers.IDLE /*left eject*/),
+  // /**
+  //  * returns the arguments to validate motor commands for a given mode
+  //  *
+  //  * @param mode the mode that the percentages apply to
+  //  * @param lowerIntakePercent lower Intake motor percent
+  //  * @param upperIntakePercent upper Intake motor percent
+  //  * @param rightEjectPercent right ejection motor percent
+  //  * @param leftEjectPercent left ejection motor percent
+  //  * @return <code>Arguments.of(Modes, MotorCommand[4])</code> to describe motor percents for a
+  //  *     given mode
+  //  */
+  // private static Arguments targetMotorPercents(
+  //     Modes mode,
+  //     double lowerIntakePercent,
+  //     double upperIntakePercent,
+  //     double rightEjectPercent,
+  //     double leftEjectPercent) {
+  //   return Arguments.of(
+  //       mode,
+  //       new double[] {lowerIntakePercent, upperIntakePercent, rightEjectPercent,
+  // leftEjectPercent});
+  // }
 
-        // // outtake, run the eject to feed the balls in and run the intake in reverse
-        // targetMotorPercents(
-        //     Modes.OUTTAKE_FAST,
-        //     IntakeRollers.OUTTAKE_LOWER_FAST /*lower intake*/,
-        //     IntakeRollers.OUTTAKE_UPPER_FAST /*upper intake*/,
-        //     EjectRollers.IDLE /*right eject*/,
-        //     EjectRollers.IDLE /*left eject*/),
+  // private static Stream<Arguments> modeMotorStatesProvider() {
+  //   return Stream.of(
+  //       // off, turn everything off
+  //       targetMotorPercents(Modes.OFF, 0 /*intake*/, 0 /*eject*/),
 
-        // // outtake for high shot, run the eject to feed the balls in and run the intake in
-        // reverse
-        // targetMotorPercents(
-        //     Modes.OUTTAKE_HIGH,
-        //     IntakeRollers.OUTTAKE_LOWER_HIGH /*lower intake*/,
-        //     IntakeRollers.OUTTAKE_UPPER_HIGH /*upper intake*/,
-        //     EjectRollers.IDLE /*right eject*/,
-        //     EjectRollers.IDLE /*left eject*/),
+  //       // idling, run only the eject motors to align balls
+  //       targetMotorPercents(Modes.IDLING, 0 /*intake*/, EjectRollers.IDLE /*eject*/),
 
-        // eject left, run the upper intake to feed the balls into the ejection rollers to expel
-        targetMotorPercents(
-            Modes.EJECT_LEFT,
-            0 /*lower intake*/,
-            IntakeRollers.INTAKE /*upper intake*/,
-            EjectRollers.IDLE /*right eject*/,
-            EjectRollers.EJECT /*left eject*/),
+  //       // intake, run the eject for alignment and the intake to hoover balls
+  //       targetMotorPercents(
+  //           Modes.INTAKE, IntakeRollers.INTAKE /*intake*/, EjectRollers.IDLE /*eject*/),
 
-        // same but eject right
-        targetMotorPercents(
-            Modes.EJECT_RIGHT,
-            0 /*lower intake*/,
-            IntakeRollers.INTAKE /*upper intake*/,
-            EjectRollers.EJECT /*right eject*/,
-            EjectRollers.IDLE /*left eject*/),
+  //       // outtake, run the eject to feed the balls in and run the intake in reverse
+  //       // targetMotorPercents(
+  //       //     Modes.OUTTAKE,
+  //       //     IntakeRollers.OUTTAKE_LOWER /*lower intake*/,
+  //       //     IntakeRollers.OUTTAKE_UPPER /*upper intake*/,
+  //       //     EjectRollers.IDLE /*right eject*/,
+  //       //     EjectRollers.IDLE /*left eject*/),
 
-        // run both ejection rollers
-        targetMotorPercents(
-            Modes.EJECT_ALL,
-            0 /*lower intake*/,
-            IntakeRollers.INTAKE /*upper intake*/,
-            EjectRollers.EJECT /*right eject*/,
-            EjectRollers.EJECT /*left eject*/));
-  }
+  //       // // outtake, run the eject to feed the balls in and run the intake in reverse
+  //       // targetMotorPercents(
+  //       //     Modes.OUTTAKE_FAST,
+  //       //     IntakeRollers.OUTTAKE_LOWER_FAST /*lower intake*/,
+  //       //     IntakeRollers.OUTTAKE_UPPER_FAST /*upper intake*/,
+  //       //     EjectRollers.IDLE /*right eject*/,
+  //       //     EjectRollers.IDLE /*left eject*/),
 
-  @RobotParamTest
-  @MethodSource("modeMotorStatesProvider")
-  public void motorStatesMatchConstantsForMode(Modes mode, double[] motorPercentArray) {
-    intakeSubsystem.setMode(mode);
-    tick();
-    for (int i = 0; i < motorArray.length; i++) {
-      double percent = motorPercentArray[i];
-      verify(
-              motorArray[i],
-              description(
-                  String.format(
-                      "mode %s should have %s running at %s percent, once",
-                      mode, motorNames[i], percent)))
-          .set(TalonFXControlMode.PercentOutput, percent);
+  //       // // outtake for high shot, run the eject to feed the balls in and run the intake in
+  //       // reverse
+  //       // targetMotorPercents(
+  //       //     Modes.OUTTAKE_HIGH,
+  //       //     IntakeRollers.OUTTAKE_LOWER_HIGH /*lower intake*/,
+  //       //     IntakeRollers.OUTTAKE_UPPER_HIGH /*upper intake*/,
+  //       //     EjectRollers.IDLE /*right eject*/,
+  //       //     EjectRollers.IDLE /*left eject*/),
 
-      // remove this line to get more useful failures for the above tests
-      verifyNoMoreInteractions(motorArray[i]);
-    }
-  }
+  //       // eject left, run the upper intake to feed the balls into the ejection rollers to expel
+  //       targetMotorPercents(
+  //           Modes.EJECT_LEFT,
+  //           0 /*lower intake*/,
+  //           IntakeRollers.INTAKE /*upper intake*/,
+  //           EjectRollers.IDLE /*right eject*/,
+  //           EjectRollers.EJECT /*left eject*/),
+
+  //       // same but eject right
+  //       targetMotorPercents(
+  //           Modes.EJECT_RIGHT,
+  //           0 /*lower intake*/,
+  //           IntakeRollers.INTAKE /*upper intake*/,
+  //           EjectRollers.EJECT /*right eject*/,
+  //           EjectRollers.IDLE /*left eject*/),
+
+  //       // run both ejection rollers
+  //       targetMotorPercents(
+  //           Modes.EJECT_ALL,
+  //           0 /*lower intake*/,
+  //           IntakeRollers.INTAKE /*upper intake*/,
+  //           EjectRollers.EJECT /*right eject*/,
+  //           EjectRollers.EJECT /*left eject*/));
+  // }
+
+  // @RobotParamTest
+  // @MethodSource("modeMotorStatesProvider")
+  // public void motorStatesMatchConstantsForMode(Modes mode, double[] motorPercentArray) {
+  //   intakeSubsystem.setMode(mode);
+  //   tick();
+  //   for (int i = 0; i < motorArray.length; i++) {
+  //     double percent = motorPercentArray[i];
+  //     verify(
+  //             motorArray[i],
+  //             description(
+  //                 String.format(
+  //                     "mode %s should have %s running at %s percent, once",
+  //                     mode, motorNames[i], percent)))
+  //         .set(TalonFXControlMode.PercentOutput, percent);
+
+  //     // remove this line to get more useful failures for the above tests
+  //     verifyNoMoreInteractions(motorArray[i]);
+  //   }
+  // }
 }
