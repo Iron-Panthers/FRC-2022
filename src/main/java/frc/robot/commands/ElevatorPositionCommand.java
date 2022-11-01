be4ff5dbcfb88530baeffe5054c3e74603cb965d
driverstation <5026craptop@gmail.com>
2022-11-01 16:39:33 -0700

    added modes to elevator subsystem and used pid value

--- src/main/java/frc/robot/commands/ElevatorPositionCommand.java
+++ src/main/java/frc/robot/commands/ElevatorPositionCommand.java
@@ -27,9 +27,7 @@ public class ElevatorPositionCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    this.elevatorSubsystem.setPercentToPidOutput();
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
