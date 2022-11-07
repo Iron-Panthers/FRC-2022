1da274df813a8caaf2b78e58670ef2b25baa37bc
Brandon <25nguyenb26@stu.smuhsd.org>
2022-11-07 12:42:52 -0800

    Using atSetpoint
    Seems like  a built-in method to detect whether robot has reached the setpoint (target height)

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -53,6 +53,7 @@ public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem() {
     heightController = new PIDController(1.5, 0.5, 0);
+    heightController.setTolerance(5,10);
 
     left_motor = new TalonFX(Constants.Elevator.Ports.LEFT_MOTOR);
     right_motor = new TalonFX(Constants.Elevator.Ports.RIGHT_MOTOR);
@@ -184,11 +185,7 @@ public class ElevatorSubsystem extends SubsystemBase {
   }
 
   public boolean atTarget() {
-    if (heightController.getPositionError() < 0.5) {
-      return true;
-    } else {
-      return false;
-    }
+    return heightController.atSetpoint();
   }
 
   /**
