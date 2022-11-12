9e8cc4edd89f2336bd9c5830b7e898de2265e091
Isaac <isaac@rubenfamily.com>
2022-11-12 15:42:35 -0800

    more work on hotchange

--- src/main/java/frc/util/HotChange.java
+++ src/main/java/frc/util/HotChange.java
@@ -1,7 +1,36 @@
 package frc.util;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleSupplier;
+
 public class HotChange {
-  public static void constant(String name, double value) {
-    System.out.println("HotChange.constant( name:" + name + ", value:" + value + ") ");
+  private HotChange() {}
+
+  private static final ShuffleboardTab tab = Shuffleboard.getTab("HotChange");
+
+  private static class Changer implements Sendable {
+    String name;
+    DoubleSupplier constGetter;
+    DoubleConsumer constSetter;
+
+    Changer(String name, DoubleSupplier constGetter, DoubleConsumer constSetter) {
+      this.name = name;
+      this.constGetter = constGetter;
+      this.constSetter = constSetter;
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+      builder.addDoubleProperty(name, constGetter, constSetter);
+    }
+  }
+
+  public static void constant(String name, DoubleSupplier constGetter, DoubleConsumer constSetter) {
+    System.out.println("HotChange.constant( name:" + name + " )");
+    tab.add(new Changer(name, constGetter, constSetter));
   }
 }
