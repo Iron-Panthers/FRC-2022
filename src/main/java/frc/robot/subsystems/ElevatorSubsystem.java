594abf930c4cf70110aff1ecba63b6e17c5705f8
driverstation <5026craptop@gmail.com>
2022-11-13 09:07:23 -0800

    added clamping slow zone for elevator pid

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -129,6 +129,27 @@ public class ElevatorSubsystem extends SubsystemBase {
     return clampPercent;
   }
 
+  private double applySlowZoneToPID(double percent) {
+    final double height = getHeight();
+    double clampPercent = MathUtil.clamp(percent, -1, 1);
+    if (
+    // going down block
+    (percent >= 0 /* going down */
+            && height <= SlowZone.LOWER_THRESHHOLD /* inside the lower slow zone */)
+        ||
+        // going up block
+        (percent <= 0 /* going up */
+            && height >= SlowZone.UPPER_THRESHHOLD /* inside the upper slow zone */)) {
+
+      // modify the output by the slowzone modifier
+
+      return MathUtil.clamp(clampPercent, -0.3, 0.3);
+
+    }
+    else {
+      return clampPercent;
+    }
+  }
   /**
    * Stores the target height for the elevator, to be reached with motor adjustment
    *
@@ -199,10 +220,10 @@ public class ElevatorSubsystem extends SubsystemBase {
     targetHeight = getTargetHeight();
     switch (currentMode) {
       case PERCENT_CONTROL:
-        percentOutput = percentControl;
-        return percentControl;
+        percentOutput = applySlowZoneToPercent(percentControl);
+        return percentOutput;
       case POSITION_CONTROL:
-        percentOutput = -heightController.calculate(currentHeight, targetHeight);
+        percentOutput = applySlowZoneToPID(-heightController.calculate(currentHeight, targetHeight));
         return percentOutput;
       default:
         return 0;
@@ -215,7 +236,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     // based on mode
     // set percent to member or pid output
 
-    right_motor.set(TalonFXControlMode.PercentOutput, applySlowZoneToPercent(getPercentControl()));
+    right_motor.set(TalonFXControlMode.PercentOutput, getPercentControl());
   }
 }
 
